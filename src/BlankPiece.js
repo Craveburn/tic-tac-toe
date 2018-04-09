366c7367740193b72caaf5d51775fec3f16e8ad4
Cameron Rabunn <raburncameron@gmail.com>
2018-04-08 20:39:40 -0600

    probably not going to use this

--- src/BlankPiece.js
+++ src/BlankPiece.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+
+class BlankPiece extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default BlankPiece
